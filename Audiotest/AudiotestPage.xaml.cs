3ab3740ca93b521b8f171653dfe4a3f0f3fa4a49
Joel Gwynn <joelman@gmail.com>
2017-02-05 09:02:07 -0500

    added.

---
+++ Audiotest/AudiotestPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AudioManager;
+using Xamarin.Forms;
+
+namespace Audiotest
+{
+	public partial class AudiotestPage : ContentPage
+	{
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			// it would be nice if this threw an exception instead of failing silently
+			Audio.Manager.PlaySound("nonexistent.mp3");
+
+			// doesn't play when first clicked on Android
+			Audio.Manager.PlaySound("tada.mp3");
+		}
+
+		public AudiotestPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using AudioManager;
+
+namespace Audiotest.Droid
+{
+	[Activity(Label = "Audiotest.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App());
+			Initializer.Initialize();
+		}
+	}
+}
